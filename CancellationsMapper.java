d51959b4242a7e1e0cc8bdf8ef4a5cfaafab1fb7
Vibhav-Ohri-vo38 <vo38@njit.edu>
2025-05-17 10:21:06 +0530

    Update CancellationsMapper.java

--- CancellationsMapper.java
+++ CancellationsMapper.java
@@ -1,8 +1,3 @@
-/**
- * @author: LIN TANG
- * @since: 2017-04-10 10:59:42 AM
- * @description: the most common reason for flight cancellations. Mapper
- */
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
@@ -10,14 +5,9 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 public class CancellationsMapper extends Mapper<Object, Text, Text, IntWritable> {
-	/**
-	 * Cancellation reasons
-	 */
 	@Override
 	protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-
 		String[] infos = value.toString().split(",");
-		// exclude the first line
 		if (!"Year".equals(infos[0])) {
 			if ("1".equals(infos[21])&&!"NA".equals(infos[22])&&infos[22].trim().length()>0) {
 				context.write(new Text(infos[22]), new IntWritable(1));
